621bfd5604d3faf345a3e19831a46896ac060783
sravanthi-paruvada <69001834+sravanthi-paruvada@users.noreply.github.com>
2021-01-13 16:09:39 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,8 +3,7 @@ public class Hello{
 	public static void main(String args[]){
 		for (int i=1;i<=10;i++){
 			System.out.println("Hello World....."+i);
-			System.out.println("Thanks for the webhooks demo..");
-			System.out.println("Thanks for the webhooks demo1..");
+		
 			System.out.println("Thanks for the webhooks demonstration..");
 			System.out.println("Thanks for the webhooks demonstration 2..");
 			System.out.println("Thanks for the webhooks demonstration 3..");
